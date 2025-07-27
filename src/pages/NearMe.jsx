5585fcb15f3f2712a456639807f97981861f0154
muhammadbilal876 <bh697397@gmail.com>
2025-07-27 18:54:41 +0500

    new version

--- src/pages/NearMe.jsx
+++ src/pages/NearMe.jsx
@@ -290,7 +290,7 @@ const macroIcons = {
 
 const containerStyle = {
   width: '100%',
-  height: 'calc(100vh - 80px)',
+  height: 'calc(100vh - 40px)',
 };
 
 function getNearestPoint(userLocation, points) {
